9bbc9a10217467cf8df3aebf4d561a6562022151
Simon Klaver <simonrklaver@gmail.com>
2020-11-06 17:32:55 +0100

    Sort of working
    ...but maybe a bit clunky. This commit is mainly a save state before pasting other html on top of it.

--- background.js
+++ background.js
@@ -1,8 +1,31 @@
+window.players = [];
+window.bids = {0: []}
+window.takes = {0: []}
+window.currentBids = [];
+window.currentRound = 1;
+
 function setEverythingToNone() {
    for (let page of ["startScreen", "playerScreen", "bidScreen", "takeScreen", "scoreBoard"]) {
       let div = document.getElementById(page);
       div.setAttribute("style", "display:none");
    }
+}
+
+function updateBidRoundLabels() {
+   let maxCards = Math.floor(52 / window.players.length);
+   let currentCards = window.currentRound;
+   if (window.currentRound === maxCards + 1) {
+      currentCards = maxCards;
+   } else if (window.currentRound > maxCards + 1) {
+      currentCards = (2 * (maxCards + 1)) - window.currentRound;
+   }
+   document.getElementById("cardsThisRoundBidForm").innerHTML = currentCards.toString();
+   document.getElementById("bidRoundNumberTitle").innerHTML = "Ronde " + window.currentRound.toString();
+}
+
+function hideUnavailableBidOptions() {
+   let bid = document.getElementById("bidScreen");
+
 }
 
 function toPlayers() {
@@ -16,6 +39,8 @@ function toBids() {
    setEverythingToNone();
    let bid = document.getElementById("bidScreen");
    bid.setAttribute("style", "display:block");
+   updateBidRoundLabels();
+   hideUnavailableBidOptions();
    return true;
 }
 
@@ -64,25 +89,46 @@ function getTypeInputFields(parent, type) {
    return parent.querySelectorAll("input[type=" + type + "]");
 }
 
-function sumOfNumberInputFields(parent) {
-   let sum = 0;
-   let inputFields = getTypeInputFields(parent, "number");
-   for (let inField of inputFields) {
-      let value = parseInt(inField.value, 10);
-      if (value > 0) {
-         sum += value;
+function clearBidsPlayer(player) {
+   let imageOff = "./images/nummer-knoppen-blauw-leeg-off.jpg";
+   let style = "background: url(\'" + imageOff + "\') center / contain no-repeat;color:"
+               + " #ffffff;";
+   for (let child of player.children) {
+      if (child.style.background.includes("nummer")) {
+         child.setAttribute("style", style);
       }
    }
-   return sum;
+}
+
+function clickBidButton(parent, number) {
+   try {
+      clearBidsPlayer(parent);
+      let numberAsInt = parseInt(number, 10);
+      window.currentBids[parent.getAttribute("playerId")] = numberAsInt;
+      updateBidsPlaced();
+      let imageToFlip = parent.children[numberAsInt + 1];
+      let imageOn = "./images/nummer-knoppen-blauw-leeg-on.jpg";
+      let style = "background: url(\'" + imageOn + "\') center / contain no-repeat;color:"
+                  + " #ffffff;";
+      imageToFlip.setAttribute("style", style);
+   } catch (e) {
+      alert(e.message);
+      return false;
+   }
+   return true;
 }
 
 function updateBidsPlaced() {
    let bidsPlaced = document.getElementById("bidsPlaced");
-   let parent = document.getElementById("bidScreen");
-   bidsPlaced.innerHTML = sumOfNumberInputFields(parent).toString();
+   let sumBids = 0;
+   for (let bid of window.currentBids) {
+      if (bid) { sumBids += bid; }
+   }
+   bidsPlaced.innerHTML = sumBids.toString();
    return true;
 }
 
 window.onload = function() {
    // Check for existing save game
+
 }
