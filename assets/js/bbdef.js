237cf50650c80ec9999d71e7285680b67cc01529
aartmeneer17 <74203941+aartmeneer17@users.noreply.github.com>
2020-11-09 18:59:29 +0100

    Export from Bootstrap Studio

--- assets/js/bbdef.js
+++ assets/js/bbdef.js
@@ -1,7 +1,51 @@
-function initSpel() {
-  // velden leegmaken
 
+
+function initSpel() {
+  var Naam = ["Aad", "Arnold", "Iduna", "Roland", "Simon"];
+
+for (j=1;j<6;j++) {
+  document.getElementById("naamKeuze-" + j).options.length = 0;
+  Naamkeuze= document.getElementById("naamKeuze-" + j);
+  for (var i = 0; i < Naam.length; i++) {
+
+    var opt = document.createElement('option');
+    opt.value = Naam[i];
+    opt.innerHTML = Naam[i];
+    Naamkeuze.appendChild(opt);
+    }
+}
+
+  // velden leegmaken
+  var d = new Date();
+document.getElementById("datumEnTijd").innerHTML = "Spel van ";
+document.getElementById("datumEnTijd").innerHTML += d.getDate();
+document.getElementById("datumEnTijd").innerHTML += " - "; document.getElementById("datumEnTijd").innerHTML += d.getMonth() + 1;
+document.getElementById("datumEnTijd").innerHTML += " - ";
+document.getElementById("datumEnTijd").innerHTML += d.getFullYear();
+document.getElementById("datumEnTijd").innerHTML += " ( ";
+document.getElementById("datumEnTijd").innerHTML += d.getHours();
+document.getElementById("datumEnTijd").innerHTML += ":";
+document.getElementById("datumEnTijd").innerHTML += d.getMinutes();
+document.getElementById("datumEnTijd").innerHTML += " ) ";
 };
+
+function namenOvernemen(){
+/* ophalen uit invoer bij Nieuw spel */
+  var gekozenNaam = ["leeg", "leeg", "leeg", "leeg", "leeg", "leeg", "leeg"];
+  for (j=1;j<6;j++) {
+    gekozenNaam[j] = document.getElementById("naamKeuze-" + j).value;
+
+  }
+
+  for (j=1;j<6;j++) {
+    document.getElementById("biedenNaam-" + j).innerHTML = gekozenNaam[j];
+    document.getElementById("halenNaam-" + j).innerHTML = gekozenNaam[j];
+    document.getElementById("standLijstNaam-" + j).innerHTML = gekozenNaam[j];    document.getElementById("eindeSpelLijstNaam-" + j).innerHTML = gekozenNaam[j];
+    }
+/* invullen in de overige schermen */
+
+
+}
 
 function clearBidsPlayer(player) {
    for (let child of player.children) {
@@ -47,6 +91,7 @@ function naarNieuwSpel() {
   alleDivsUit ();
   // velden leegmaken
   document.getElementById('nieuwSpel').style.display='block';
+  initSpel();
 };
 
 function naarEindeSpel() {
@@ -71,6 +116,8 @@ function naarHalen() {
 function naarStand() {
   alleDivsUit ();
   // velden verversen
+
+  // of gegevens ophalen uit eerder opgeslagen bestand
   document.getElementById('stand').style.display='block';
 };
 
