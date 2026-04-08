8eaebad415aa881a1ec2cc55d1f970f53f13afda
Simon R Klaver <Justist@users.noreply.github.com>
2026-04-08 10:42:27 +0200

    Added firefox and safari as test browsers

--- playwright.config.js
+++ playwright.config.js
@@ -26,6 +26,14 @@ module.exports = defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
   ],
 });
 
